4ad428ed4410ccdd155fe775db95e82ba8bee43b
HieuCaoTlu <trunghieu.personal@gmail.com>
2025-04-02 10:10:59 +0700

    feat: Thêm API huấn luyện qua video
    - Mặc định lấy 30 tấm ảnh của video
    - Giảm thời gian xác minh nhân viên xuống 2 giây
    - Yêu cầu nhân viên đứng trước camera gần hơn ~10cm
    - Tạo nhãn giả để softmax chạy được với 2 nhãn trở lên
    
    Signed-off-by: HieuCaoTlu <trunghieu.personal@gmail.com>

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, APIRouter, Depends, File, UploadFile, Form
-from database import session, Employee, get_date, Attendance, get_time, time_to_string
+from fastapi import APIRouter, Form, UploadFile, File
+from database import session, Employee, Complaint
+from camera import generate_complaint_camera, stop_camera, train_via_video
 from utils.speech import text_to_speech
-from typing import List
-from ai import train
-from sqlalchemy import func
+from utils.date import get_accruate
+import uuid
+import sys
+import os
 
 router = APIRouter()
 
@@ -26,6 +28,61 @@ async def add_employee(
         "employee_id": employee_id,
     }
 
-@router.post("/speech")
+@router.post("/speech", tags=["Utils"])
 async def speech(text: str = Form(...)):
     return {"audio": text_to_speech(text)}
+
+@router.get("/complaint", tags=["Complaint"])
+async def get_complaint():
+    return {"image": generate_complaint_camera()}
+
+@router.post("/complaint", tags=["Complaint"])
+async def add_complaint(
+    image: UploadFile = File(...),
+    employee_id: int = Form(...),
+    reason: str = Form(...)
+):
+    image_bytes = await image.read()
+    complaint = Complaint(employee_id=employee_id, reason=reason, image=image_bytes)
+    session.add(complaint)
+    session.commit()
+    session.refresh(complaint)
+    return {
+        "id": complaint.id,
+        "created_at": complaint.created_at.isoformat(),
+        "reason": complaint.reason
+    }
+
+@router.get("/employee", tags=["Employee"])
+async def employees():
+    employees = session.query(Employee.id, Employee.name).all()
+    employee_list = [{"id": emp.id, "name": emp.name} for emp in employees]
+    return {"employees": employee_list}
+
+@router.get("/date", tags=["Utils"])
+async def get_date():
+    return {"date": get_accruate()}
+
+@router.get("/exit", tags=["Utils"])
+async def exit():
+    stop_camera()
+    sys.exit(0)
+    exit()
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+@router.post("/train_video", tags=["Utils"])
+async def train_video(video: UploadFile = File(...), name: str = Form(...),position: str = Form(...)):
+    new_employee = Employee(name=name, position=position)
+    session.add(new_employee)
+    session.commit()
+    session.refresh(new_employee)
+    employee_id = new_employee.id
+    video_path = os.path.join(UPLOAD_FOLDER, f"{uuid.uuid4()}.mp4")
+    with open(video_path, "wb") as buffer:
+        buffer.write(await video.read())
+    result = train_via_video(video_path, employee_id)
+    os.remove(video_path)
+    if result:
+        return True
+    return False
